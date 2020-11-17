ddd5a667cfcede30cdd3e33aa06c278c94088674
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-11-17 19:12:48 +0700

    fix

--- src/views/error/NotFound.vue
+++ src/views/error/NotFound.vue
@@ -5,7 +5,7 @@
                 h1.error.mx-auto 404
                 p.lead.mb-5 {{ $t('error.not_found._') }}
                 p.mb-0 {{ $t('error.not_found.desc') }}
-                router-link(:to="{path: '/'}") {{ $t('error.back_to_root') }}
+                router-link(:to="appRoutes.root") {{ $t('error.back_to_root') }}
 </template>
 
 <script>
@@ -14,9 +14,15 @@
  */
 
 import {headTitle} from '../../app/utils'
+import {APP_ROUTE} from '@/app/config'
 
 export default {
     name: 'NotFound',
+    data() {
+        return {
+            appRoutes: APP_ROUTE,
+        }
+    },
     head: {
         title() {
             return {
