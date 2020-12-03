fb9b7342249b69952ed85a5a617259a3952eacbe
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2020-12-04 01:44:43 +0700

    fix

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -2,9 +2,10 @@
  * Base - Any modification needs to be approved, except the space inside the block of TODO
  */
 
-import routePermissions from './route-permissions'
 import {cacheHandler, permit, PermissionBarrier} from '@/app/utils'
+import routePermissions from './route-permissions'
 
-export {routes} from './routes'
+export * from './routes'
+export {routePermissions}
 export const permissionBarrier = new PermissionBarrier(permit, cacheHandler)
     .importFromRoutePermissions(routePermissions)
