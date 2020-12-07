e01da9b84be6864f8addf2dec84b0f2fd05aa5dc
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2020-12-07 16:44:16 +0700

    fix

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -9,14 +9,25 @@ import {APP_DEFAULT_SERVICE} from '@/app/config'
 
 export const serviceFactory = new ServiceFactory(services, defaultService)
 
-export const defaultServiceModifyHeader = (name, value) => {
+export const defaultServiceModifyHeader = (name, value, permanent = false) => {
     serviceFactory.modify(defaultServiceInstance => defaultServiceInstance.addInstanceCallback(name, instance => {
-        if (typeof value !== 'string') {
-            value = JSON.stringify(value)
+        if (value) {
+            if (typeof value !== 'string') {
+                value = JSON.stringify(value)
+            }
+
+            if (name in instance.defaults.headers) {
+                delete instance.defaults.headers[name]
+            }
+            instance.defaults.headers.common[name] =
+                name === APP_DEFAULT_SERVICE.headerTokenAuthorization || APP_DEFAULT_SERVICE.headerEncryptExcepts.includes(name) ?
+                    value : crypto.encrypt(value)
+        } else {
+            instance.defaults.headers.common[name] = null
+            if (permanent) {
+                instance.defaults.headers[name] = ''
+            }
         }
-        instance.defaults.headers.common[name] =
-            name === APP_DEFAULT_SERVICE.headerTokenAuthorization || APP_DEFAULT_SERVICE.headerEncryptExcepts.includes(name) ?
-                value : crypto.encrypt(value)
         return instance
     }))
 }
